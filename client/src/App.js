4aa277b3abfbcc86c760616052ec833a89bf6729
Jintu-sudo <jin2.b@prton.me>
2025-05-25 14:08:46 +0530

    Some changes...

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+
+const Home = () => (
+  <div style={{ padding: "20px", textAlign: "center" }}>
+    <h1>Welcome to Homeschool Hub</h1>
+    <p>
+      Please <a href="/login">Login</a> or <a href="/register">Register</a>
+    </p>
+  </div>
+);
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
--- server/index.js
+++ server/index.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
